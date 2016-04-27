6a165667c8de32ce7335a37266d9f91db6b47bb5
tsaghir <tsaghir@foi.hr>
2016-04-27 20:32:22 +0200

    BLL edit, DAL edit, new forms, 1st query
    [EDIT]App config: connection string added
    [ADD]frmLogin: prototype login screen (login data: Ana)
    [ADD]frmMenu: added new form which will be the menu for the utilities
    [ADD]frmRecipes: started with my 1st utility
    --------------------------------------
    DAL
    [ADD]djelatniciDAL: new class for CRUD methods for djelatnici objects
    ---------------------------------------------------
    BLL
    [ADD]GetData: class for getting the data from all the objects.

--- HouseMed/HouseMed/frmLogin.cs
+++ HouseMed/HouseMed/frmLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HouseMed.BAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,43 @@ namespace HouseMed
 {
     public partial class frmLogin : Form
     {
+        #region Variables
+        // instance to access the methods
+        GetData djelatniciData = new GetData();
+        #endregion
+
+        #region Constructor
         public frmLogin()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Button methods
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var djelatniciLista = djelatniciData.GetDjelatnici();
+
+            foreach (var it in djelatniciLista)
+            {
+                if (it.ime == txtUser.Text)
+                {
+                    using (frmMenu frm = new frmMenu())
+                    {
+                        this.Hide();
+                        frm.ShowDialog();
+                    }
+                    this.Show();
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("Unijeli ste krive podatke\n");
+                    sb.Append("Testni login: Ime = Ana");
+                    MessageBox.Show(sb.ToString());
+                }
+            }
+        }
+        #endregion
     }
 }
---
+++ HouseMed/HouseMed/frmMenu.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace HouseMed
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkarton = new System.Windows.Forms.Button();
+            this.btnLijekovi = new System.Windows.Forms.Button();
+            this.btnRecept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEkarton
+            // 
+            this.btnEkarton.Location = new System.Drawing.Point(145, 304);
+            this.btnEkarton.Name = "btnEkarton";
+            this.btnEkarton.Size = new System.Drawing.Size(83, 37);
+            this.btnEkarton.TabIndex = 0;
+            this.btnEkarton.Text = "E - karton";
+            this.btnEkarton.UseVisualStyleBackColor = true;
+            // 
+            // btnLijekovi
+            // 
+            this.btnLijekovi.Location = new System.Drawing.Point(248, 304);
+            this.btnLijekovi.Name = "btnLijekovi";
+            this.btnLijekovi.Size = new System.Drawing.Size(83, 37);
+            this.btnLijekovi.TabIndex = 1;
+            this.btnLijekovi.Text = "Lijekovi";
+            this.btnLijekovi.UseVisualStyleBackColor = true;
+            // 
+            // btnRecept
+            // 
+            this.btnRecept.Location = new System.Drawing.Point(38, 304);
+            this.btnRecept.Name = "btnRecept";
+            this.btnRecept.Size = new System.Drawing.Size(83, 37);
+            this.btnRecept.TabIndex = 2;
+            this.btnRecept.Text = "Izrada recepata";
+            this.btnRecept.UseVisualStyleBackColor = true;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 390);
+            this.Controls.Add(this.btnRecept);
+            this.Controls.Add(this.btnLijekovi);
+            this.Controls.Add(this.btnEkarton);
+            this.Name = "frmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Funkcionalnosti";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkarton;
+        private System.Windows.Forms.Button btnLijekovi;
+        private System.Windows.Forms.Button btnRecept;
+    }
+}
