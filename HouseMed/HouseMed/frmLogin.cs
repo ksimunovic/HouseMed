f6612421f3dd59f4cc44eac10f6555080dc3f2e0
tsaghir <tsaghir@foi.hr>
2016-06-08 02:47:36 +0200

    test staged

---
+++ HouseMed/HouseMed/frmLogin.cs
@@ -0,0 +1,55 @@
+﻿using HouseMed.BAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HouseMed
+{
+    public partial class frmLogin : Form
+    {
+        #region Variables
+        // instance to access the methods
+        GetData djelatniciData = new GetData();
+        #endregion
+
+        #region Constructor
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Button methods
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var djelatniciLista = djelatniciData.GetDjelatnici();
+
+            foreach (var it in djelatniciLista)
+            {
+                if (it.ime == txtUser.Text)
+                {
+                    using (frmMenu frm = new frmMenu())
+                    {
+                        this.Hide();
+                        frm.ShowDialog();
+                    }
+                    this.Show();
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("Unijeli ste krive podatke\n");
+                    sb.Append("Testni login: Ime = Ana");
+                    MessageBox.Show(sb.ToString());
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# r16049 HouseMed
+
+Ovdje se nalazi programsko rješenje za desktop aplikaciju HouseMed
+
+Ukoliko ste zainteresirani za više informacije prolistajte kroz naš Wiki.
+
+Hvala,
+
+HouseMed tim!
